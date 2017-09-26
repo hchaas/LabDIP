ec0b9daabfb270e48e08da1faab5623aeb3ee47e
User <User@10.30.23.12>
2017-09-25 21:46:59 -0500

    finished lab 2 baggage service, started on food service

--- src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
@@ -1,7 +1,7 @@
-
 package dip.lab2.student.solution1;
 
 public class BaggageServiceTipCalculator {
+
     private double minBill = 0.00;
     private double maxBill = 100.00;
     private double goodRate = 0.20;
@@ -10,7 +10,7 @@ public class BaggageServiceTipCalculator {
     private double baseTipPerBag;
     private int bagCount;
     private ServiceQuality serviceQuality;
-    
+
     public BaggageServiceTipCalculator(ServiceQuality q, int bags) {
         this.setServiceRating(q); // perform validation
         this.setBagCount(bags);
@@ -18,77 +18,103 @@ public class BaggageServiceTipCalculator {
         baseTipPerBag = 1.00; // set default value
     }
 
-    public double getMinBill(){
+    public final double getMinBill() {
         return minBill;
     }
-    
-    public void setMinBill(double minBill){
-        if (minBill <0){
+
+    public final void setMinBill(double minBill) {
+        if (minBill < 0) {
             throw new IllegalArgumentException("Minimum bill amount cannot be less than zero.");
-        }
-        else{
+        } else {
             this.minBill = minBill;
         }
     }
-    
-    public double getMaxBill(){
+
+    public final double getMaxBill() {
         return maxBill;
     }
-    
-    public void setMaxBill(double maxBill){
-        if (maxBill == 0 || maxBill <0){
+
+    public final void setMaxBill(double maxBill) {
+        if (maxBill == 0 || maxBill < 0) {
             throw new IllegalArgumentException("Maximum bill amount cannot be less than or equal to zero.");
-        }
-        else{
+        } else {
             this.maxBill = maxBill;
         }
     }
-    
-    public double getGoodRate(){
+
+    public final double getGoodRate() {
         return goodRate;
     }
-    
-    public void setGoodRate(double goodRate){
-        if (goodRate == 0 || goodRate<0 || goodRate>1){
-        throw new IllegalArgumentException("Rate must be between zero and 1.");
-        }
-        else{
+
+    public final void setGoodRate(double goodRate) {
+        if (goodRate == 0 || goodRate < 0 || goodRate > 1) {
+            throw new IllegalArgumentException("Rate must be between zero and 1.");
+        } else {
             this.goodRate = goodRate;
         }
     }
-    
-    public double getFairRate(){
+
+    public final double getFairRate() {
         return fairRate;
     }
-    public void setFairRate (double fairRate){
-        if(fairRate ==0 || fairRate<0 || fairRate>1){
+
+    public final void setFairRate(double fairRate) {
+        if (fairRate == 0 || fairRate < 0 || fairRate > 1) {
             throw new IllegalArgumentException("Rate must be between zero and one.");
-        }
-        else{
+        } else {
             this.fairRate = fairRate;
         }
     }
-    
-    public double getPoorRate(){
+
+    public final double getPoorRate() {
         return poorRate;
     }
-    
-    public void setPoorRate(double poorRate){
-        if(poorRate <0 || fairRate>1){
+
+    public final void setPoorRate(double poorRate) {
+        if (poorRate < 0 || fairRate > 1) {
             throw new IllegalArgumentException("Rate cannot be less than zero or more than one.");
-        }
-        else{
+        } else {
             this.poorRate = poorRate;
         }
     }
-    
-    
-    
-    
-    public double getTip() {
+
+    public final double getBaseTipPerBag() {
+        return baseTipPerBag;
+    }
+
+    public final void setBaseTipPerBag(double baseTipPerBag) {
+        if (baseTipPerBag < 0 || baseTipPerBag == 0) {
+            throw new IllegalArgumentException(
+                    "error: base tip must be greater than or equal to zero");
+        }
+        this.baseTipPerBag = baseTipPerBag;
+    }
+
+    public final int getBagCount() {
+        return bagCount;
+    }
+
+    public final void setBagCount(int bagCount) {
+        if (bagCount < 0 || bagCount == 0) {
+            throw new IllegalArgumentException(
+                    "bag count must be greater than or equal to zero");
+        }
+        this.bagCount = bagCount;
+    }
+
+    public final void setServiceRating(ServiceQuality q) {
+        // No need to validate because enums provide type safety!
+        serviceQuality = q;
+    }
+
+    public final ServiceQuality getServiceQuality() {
+        return serviceQuality;
+    }
+
+    public final double getTip() {
         double tip = 0.00; // always initialize local variables
 
-        switch(serviceQuality) {
+        switch (serviceQuality) {
             case GOOD:
                 tip = baseTipPerBag * bagCount * (1 + goodRate);
                 break;
@@ -101,38 +127,5 @@ public class BaggageServiceTipCalculator {
         }
 
         return tip;
-    }
-
-    public final void setServiceRating(ServiceQuality q) {
-        // No need to validate because enums provide type safety!
-        serviceQuality = q;
-    }
-
-    public ServiceQuality getServiceQuality() {
-        return serviceQuality;
-    }
-
-    public int getBagCount() {
-        return bagCount;
-    }
-
-    public final void setBagCount(int bagCount) {
-        if(bagCount < 0 || bagCount == 0) {
-            throw new IllegalArgumentException(
-                    "bag count must be greater than or equal to zero");
-        }
-        this.bagCount = bagCount;
-    }
-
-    public double getBaseTipPerBag() {
-        return baseTipPerBag;
-    }
-
-    public void setBaseTipPerBag(double baseTipPerBag) {
-        if(baseTipPerBag < 0 || baseTipPerBag ==0) {
-            throw new IllegalArgumentException(
-                    "error: base tip must be greater than or equal to zero");
-        }
-        this.baseTipPerBag = baseTipPerBag;
     }
 }
--- src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -2,17 +2,13 @@
 package dip.lab2.student.solution1;
 
 public class FoodServiceTipCalculator {
-    private static final double MIN_BILL = 0.00;
-    private static final String BILL_ENTRY_ERR =
+    private double MIN_BILL = 0.00;
+    private String BILL_ENTRY_ERR =
             "Error: bill must be greater than or equal to " + MIN_BILL;
-    private static final double GOOD_RATE = 0.20;
-    private static final double FAIR_RATE = 0.15;
-    private static final double POOR_RATE = 0.10;
-
+    private double GOOD_RATE = 0.20;
+    private double FAIR_RATE = 0.15;
+    private double POOR_RATE = 0.10;
     private double bill;
-    public enum ServiceQuality {
-        GOOD, FAIR, POOR
-    }
     private ServiceQuality serviceQuality;
 
     public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
