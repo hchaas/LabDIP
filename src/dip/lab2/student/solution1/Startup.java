77428bba593a7be61c2a187e8fd8418a3e10423e
User <User@10.30.23.12>
2017-09-26 22:00:30 -0500

    completed service and startup classes

--- src/dip/lab2/student/solution1/Startup.java
+++ src/dip/lab2/student/solution1/Startup.java
@@ -1,11 +1,6 @@
 package dip.lab2.student.solution1;
 
 public class Startup {
-//            AnimalAgeCalculator animalAge = new CatAgeCalculator(10);       
-//        AnimalAgeService animalAgeService = new AnimalAgeService(animalAge);
-//        double humanAge = animalAgeService.calculateHumanAge(10);
-//        
-//        System.out.println("The human age equivalent of this animal is " + humanAge + " years.");
 
     public static void main(String[] args) {
         TipCalculator newTip = new BaggageServiceTipCalculator(ServiceQuality.GOOD, 55);
--- src/dip/lab3/student/solution1/Startup.java
+++ src/dip/lab3/student/solution1/Startup.java
@@ -3,6 +3,18 @@ package dip.lab3.student.solution1;
 
 public class Startup {
     public static void main(String[] args) {
+//        TipCalculator newTip = new BaggageServiceTipCalculator(ServiceQuality.GOOD, 55);
+//        TipCalculatorService tipThis = new TipCalculatorService();
+//        double thisTip = tipThis.getTip(newTip);
+        
+//        System.out.println("Tip is: " + thisTip);
+        Reader reader = new KeyboardInput();
+        MessageService thisReader = new MessageService();
+        String myMessage = thisReader.readMessage(reader);
+        
+        Writer writer = new GUIOutput();
+        MessageService thisWriter = new MessageService();
+        thisWriter.writeMessage(writer);
         
     }
 }
