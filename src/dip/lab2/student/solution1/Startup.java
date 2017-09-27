6fa0ee318d4bdc1bc2bef3b0dc30971d406fca0d
User <User@10.30.23.12>
2017-09-26 20:47:41 -0500

    finished lab2

--- src/dip/lab2/student/solution1/Startup.java
+++ src/dip/lab2/student/solution1/Startup.java
@@ -1,4 +1,3 @@
-
 package dip.lab2.student.solution1;
 
 public class Startup {
@@ -7,10 +6,13 @@ public class Startup {
 //        double humanAge = animalAgeService.calculateHumanAge(10);
 //        
 //        System.out.println("The human age equivalent of this animal is " + humanAge + " years.");
-    
-    TipCalculator newTip = new FoodServiceTipCalculator(ServiceQuality.GOOD, 55);
-    TipCalculatorService tipThis = new TipCalculatorService();
-    double thisTip = tipThis.getTip(newTip);
-    
-    
+
+    public static void main(String[] args) {
+        TipCalculator newTip = new BaggageServiceTipCalculator(ServiceQuality.GOOD, 55);
+        TipCalculatorService tipThis = new TipCalculatorService();
+        double thisTip = tipThis.getTip(newTip);
+        
+        System.out.println("Tip is: " + thisTip);
+    }
+
 }
